db612f888c5f76d0087e7d33d09432fb9f9506aa
Nathan Friedly <nathan@nfriedly.com>
2017-05-26 17:18:28 -0400

    formatting

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ const firebaseConfig = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-    // A bucket is a container for objects (files).
+  // A bucket is a container for objects (files).
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID
 };
 // [END config]
 
-app.get('/', function(req, res, next){
-    res.render('index', {firebaseConfig});
+app.get('/', function(req, res, next) {
+  res.render('index', {firebaseConfig});
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
